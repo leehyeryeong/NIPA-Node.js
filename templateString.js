1002c0c20cf83cdff22b9251dcc09bf9098aad31
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-28 09:34:58 +0900

    ES6 template string 사용하기

---
+++ templateString.js
@@ -0,0 +1,9 @@
+const animal = 'Cat';
+const legs = 4;
+const sound = 'meow';
+
+/* 지시사항의 내용을 따라 아래 코드를 수정해보세요. */
+const explain = `${animal} has ${legs} legs.
+                 ${animal} says "${sound}".`;
+    
+console.log(explain);
