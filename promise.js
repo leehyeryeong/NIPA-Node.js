b84bfd036b1f2025b1f3b07c2f130508bd4e1eb5
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-30 01:00:19 +0900

    오류 수정

--- promise.js
+++ promise.js
@@ -21,7 +21,7 @@ function adder_promise(a, b) {
                 return;
             }
             /* 2. promise 에서 결과값 처리하기 */
-            resolve(a+b);
+            resolve(a + b);
         });
     });
 }
@@ -30,14 +30,14 @@ function add_all(a, b, c) {
     adder_promise(a, b)
         /* 3. then 을 활용하여 result 와 c 를 add_promise 하기 */
         .then(result => {
-            return adder_promise(result, c);
+            return adder_promise(result, c)
         })
         .then(result2 => {
             console.log(`${a}+${b}+${c}=${result2}`);
         })
         /* 4. catch 를 활용하여 promise 의 에러를 handle_error 함수로 전달하기 */
         .catch(
-            error => handle_error(error);
+            error => handle_error(error)
         )
 }
 
