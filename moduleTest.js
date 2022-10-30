e2ced457473c0b6f58e0589eba1784d0aa483c32
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-30 23:58:25 +0900

    여러 모듈 사용해보는 실습

---
+++ moduleTest.js
@@ -0,0 +1,12 @@
+const module1 = require('./module1');
+const module2 = require('./module2');
+const data = require('./data');
+const funcModule = require('./funcModule');
+
+console.log(
+    module1,
+    module2,
+    data.name,
+    /* funcModule 사용 */
+    funcModule()
+);
