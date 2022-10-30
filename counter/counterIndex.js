fe9d6f97d874c71c1dc300735ba88d7c90972f71
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-30 23:37:53 +0900

    사용할 때마다 숫자를 하나씩 카운트하는 모듈을 작성하고자 하는 코드

---
+++ counter/counterIndex.js
@@ -0,0 +1,6 @@
+const counter = require('./counter');
+
+for (let i = 0; i < 10; i++) {
+    /* 지시사항 2번 내용을 따라 코드를 수정해보세요. */
+    console.log(counter());
+}
---
+++ counter/counterModule.js
@@ -0,0 +1,7 @@
+let count = 0;
+
+/* 지시사항 1번 내용을 내용을 따라 코드를 수정해보세요. */
+module.exports = () => {
+    count += 1;
+    return count;
+};
